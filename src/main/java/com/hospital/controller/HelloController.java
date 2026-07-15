0f89e056d11eacee48bbd665f9e83db18358d233
Mqiuqiu77 <1648817167@qq.com>
2026-07-15 18:03:30 +0800

    feat:新增病人信息查询接口

--- src/main/java/com/hospital/controller/HelloController.java
+++ src/main/java/com/hospital/controller/HelloController.java
@@ -1,13 +1,18 @@
 package com.hospital.controller;
 
+import com.hospital.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
 
+    @Autowired
+    private UserService userService;
+
     @GetMapping("/hello")
     public String hello(){
-        return "Hello SpringBoot";
+        return userService.login();
     }
 }
