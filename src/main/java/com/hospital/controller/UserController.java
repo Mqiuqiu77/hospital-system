af0fffff8af3e016746223839e453d764ec4a9f9
Mqiuqiu77 <1648817167@qq.com>
2026-07-15 21:55:50 +0800

    Return user info as response DTO

--- src/main/java/com/hospital/controller/UserController.java
+++ src/main/java/com/hospital/controller/UserController.java
@@ -1,6 +1,6 @@
 package com.hospital.controller;
 
-
+import com.hospital.dto.UserInfoResponse;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -11,8 +11,8 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
 
     @GetMapping("/info")
-    public String getInfo(@RequestParam Integer id,
-                          @RequestParam String name){
-        return "用户编号:" + id + "用户名:" + name;
+    public UserInfoResponse getInfo(@RequestParam Integer id,
+                                    @RequestParam String name) {
+        return new UserInfoResponse(id, name);
     }
 }
---
+++ src/main/java/com/hospital/dto/UserInfoResponse.java
@@ -0,0 +1,20 @@
+package com.hospital.dto;
+
+public class UserInfoResponse {
+
+    private final Integer id;
+    private final String name;
+
+    public UserInfoResponse(Integer id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
